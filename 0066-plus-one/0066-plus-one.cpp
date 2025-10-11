3527fa25a3fa8307d25fde2b777cc987dd060619
AVVARU YASASVI <cs1240063@iitd.ac.in>
2025-10-11 22:53:54 +0530

    Time: 0 ms (100%), Space: 11.5 MB (47.22%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,27 +3,26 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
         reverse(digits.begin(),digits.end());
-        vector<int>v=digits;
         int carry=1;
         for(int i=0;i<n;i++){
-            if(carry==1 and carry+v[i]<=9){
-                v[i]+=carry;
+            if(carry==1 and digits[i]<9){
+                digits[i]+=carry;
                 break;
             }
-            else if(carry==1 and v[i]==9){
+            else if(carry==1 and digits[i]==9){
                 if(i==n-1){
-                    v[i]=0;
-                    v.push_back(1);
+                    digits[i]=0;
+                    digits.push_back(1);
                 }
                 else{
-                    v[i]=0;
+                    digits[i]=0;
                 }
             }
             else{
                 break;
             }
         }
-        reverse(v.begin(),v.end());
-        return v;
+        reverse(digits.begin(),digits.end());
+        return digits;
     }
 };
