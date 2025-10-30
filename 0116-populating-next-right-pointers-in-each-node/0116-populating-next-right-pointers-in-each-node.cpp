ef21e1173aa53178d77f0145a79cc09d3beff846
AVVARU YASASVI <cs1240063@iitd.ac.in>
2025-10-30 18:24:53 +0530

    Time: 12 ms (67.55%), Space: 19.2 MB (43.09%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -19,24 +19,30 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root)return NULL;
-        queue<Node*>q;
+        if (!root) return NULL;
+        
+        queue<Node*> q;
         q.push(root);
-        Node* temp;
-        int sz=0;
-        while(!q.empty()){
-            sz=q.size();
-            queue<Node*>q1;
-            for(int i=0;i<sz;i++){
-            temp=q.front();
-            q.pop();
-            q.empty()?temp->next=NULL:temp->next=q.front();
-            if(temp->left)q1.push(temp->left);
-            if(temp->right)q1.push(temp->right);
-            }
-            while(!q1.empty()){
-                q.push(q1.front());
-                q1.pop();
+        
+        while (!q.empty()) {
+            int sz = q.size(); // Get the size of the *current* level
+            
+            for (int i = 0; i < sz; i++) {
+                Node* temp = q.front();
+                q.pop();
+                
+                // Link to the next node *in the queue* (which is on the same level)
+                // If it's the last node of the level, q.front() will be from the *next* level,
+                // so we only link if i < sz - 1.
+                if (i == sz - 1) {
+                    temp->next = NULL;
+                } else {
+                    temp->next = q.front();
+                }
+                
+                // Add children to the *back* of the same queue for the *next* level
+                if (temp->left) q.push(temp->left);
+                if (temp->right) q.push(temp->right);
             }
         }
         return root;
