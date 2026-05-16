1528f295c908e9877db5105c418cb7434bcb1216
AVVARU YASASVI <cs1240063@iitd.ac.in>
2026-05-16 22:55:34 +0530

    Time: 0 ms (100%), Space: 10 MB (57.48%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-
-        for(int mask = 0; mask < (1 << n); mask++) {
-
-            vector<int> subset;
-
-            for(int j = 0; j < n; j++) {
-
-                // check if j-th bit is set
-                if(mask & (1 << j)) {
-                    subset.push_back(nums[j]);
-                }
+        int n=nums.size();
+        vector<vector<int>>ans;
+        for(int mask=0;mask<(1<<n);mask++){
+            vector<int>v;
+            for(int j=0;j<n;j++){
+                if(mask & (1<<j))v.push_back(nums[j]);
             }
-
-            ans.push_back(subset);
+            ans.push_back(v);
         }
-
         return ans;
     }
 };
