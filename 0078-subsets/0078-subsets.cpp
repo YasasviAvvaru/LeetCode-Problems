1c49d6115454a9f8142bf4b8bd96ac195957f893
AVVARU YASASVI <cs1240063@iitd.ac.in>
2026-05-16 22:50:42 +0530

    Time: 0 ms (100%), Space: 10 MB (36.71%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
-    string cvt2bin(int n,int sz){
-        string s;
-        if(n==0){
-            
-        for(int i=0;i<sz;i++){
-            s.push_back('0');
-        }
-        return s;
-        }
-        while(n>1){
-            if(n%2==0){
-                s.push_back('0');
-            }
-            else{
-                s.push_back('1');
-            }
-            n/=2;
-        }
-        s.push_back('1');
-        while(s.size()<sz){
-            s.push_back('0');
-        }
-        reverse(s.begin(),s.end());
-        return s;
-    }
-    void fxn(int i,vector<int>& nums,vector<vector<int>>& ans){
-        vector<int>v;
-        int n=nums.size();
-        string s=cvt2bin(i,n);
-        for(int j=n-1;j>=0;j--){
-            if(s[j]=='1')v.push_back(nums[j]);
-        }
-        ans.push_back(v);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>ans;
-        for(int i=0;i<(1<<n);i++){
-            fxn(i,nums,ans);
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        for(int mask = 0; mask < (1 << n); mask++) {
+
+            vector<int> subset;
+
+            for(int j = 0; j < n; j++) {
+
+                // check if j-th bit is set
+                if(mask & (1 << j)) {
+                    subset.push_back(nums[j]);
+                }
+            }
+
+            ans.push_back(subset);
         }
+
         return ans;
     }
 };
