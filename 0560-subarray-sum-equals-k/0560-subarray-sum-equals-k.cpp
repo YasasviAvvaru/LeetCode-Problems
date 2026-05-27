87033db0415181d93d8e6025c12b23f3dbc33552
AVVARU YASASVI <cs1240063@iitd.ac.in>
2026-05-27 18:22:43 +0530

    Time: 48 ms (44.77%), Space: 51 MB (18.42%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& v, int k) {
-        int n = v.size();
+    int subarraySum(vector<int>& nums, int k) {
 
-        vector<int> pf(n + 1, 0);
+        unordered_map<int,int> freq;
 
-        for(int i = 0; i < n; i++) {
-            pf[i + 1] = pf[i] + v[i];
+        freq[0] = 1;
+
+        int sum = 0;
+        int ans = 0;
+
+        for(int& x : nums) {
+
+            sum += x;
+
+            ans += freq[sum - k];
+
+            freq[sum]++;
         }
 
-        unordered_map<int, vector<int>> pos;
-
-        for(int j = 0; j <= n; j++) {
-            pos[pf[j]].push_back(j);
-        }
-
-        int ct = 0;
-
-        for(int i = 0; i < n; i++) {
-
-            auto &z = pos[pf[i] + k];
-
-            ct += z.end() - upper_bound(z.begin(), z.end(), i);
-        }
-
-        return ct;
+        return ans;
     }
 };
