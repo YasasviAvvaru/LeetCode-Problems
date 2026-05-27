84980d11e18326f2b5bcec97d62933ceffcc028e
AVVARU YASASVI <cs1240063@iitd.ac.in>
2026-05-27 15:22:08 +0530

    Time: 11 ms (30.05%), Space: 128.2 MB (77.75%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& v) {
         int sz=v.size();
-        vector<int>p,n;
-        for(auto& x:v){
-            if(x<0)n.push_back(x);
-            else p.push_back(x);
+        vector<int>ans(sz,0);
+        int p=0,n=1;
+        for(int i=0;i<sz;i++){
+            if(v[i]>0){
+                ans[p]=v[i];
+                p+=2;
+            }
+            else{
+                ans[n]=v[i];
+                n+=2;
+            }
         }
-        for(int i=0;i<sz/2;i++){
-            v[2*i]=p[i];
-            v[2*i+1]=n[i];
-        }
-        return v;
+        return ans;
     }
 };
