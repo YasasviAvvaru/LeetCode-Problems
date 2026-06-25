d6efde1dc6708ebe332f04638ce4dd1745d410f6
AVVARU YASASVI <cs1240063@iitd.ac.in>
2026-06-26 03:43:56 +0530

    Time: 0 ms (100%), Space: 31.3 MB (45.88%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n=nums.size(),mid=0,st=0,end=n-1;
-        while(st<=end){
-            mid=(st+end)/2;
-            if(nums[mid]<target){
-                st=mid+1;
-            }
-            else if(nums[mid]==target){
-                return mid;
+    bool isvalid(vector<int>& v,int idx,int val){
+        return (v[idx]>=val);
+    }
+    int search(vector<int>& v, int target) {
+        //bool fxn is f(val)= (val >= target.)
+        //we find first True.
+        //if(ans==target)then its idx.
+        //else -1;
+
+        //if all are <target->-1
+        int l=0,r=v.size()-1,mid,ans=-1;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(isvalid(v,mid,target)){
+                ans=mid;
+                r=mid-1;
             }
             else{
-                end=mid-1;
+                l=mid+1;
             }
         }
-        if(end>=0){
-            if(nums[end]==target)return end;
-            else return -1;
-        }
-        return -1;
+        if(ans==-1)return -1;
+        if(v[ans]>target)return -1;
+        return ans;
     }
 };
