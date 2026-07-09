122c749671118c9f397ad7ae8d65081169d711a2
AVVARU YASASVI <cs1240063@iitd.ac.in>
2026-07-09 15:05:38 +0530

    Time: 0 ms (100%), Space: 101.5 MB (5.18%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& v) {
-        //buy at low and sell at high.
-        //for each idx i find its sf_max.
-        //ans=for all i max(sf_max[i]-v[i]);
+        stack<int>st;
         int n=v.size();
-        vector<int>sf(n,0);
+        st.push(n-1);
+        vector<int>sf(n);
         sf[n-1]=v[n-1];
         for(int i=n-2;i>=0;i--){
-            sf[i]=max(sf[i+1],v[i+1]);
+            sf[i]=max(sf[i+1],v[i]);
         }
         int ans=0;
         for(int i=0;i<n-1;i++){
-            ans=max(ans,sf[i]-v[i]);
+            ans=max(ans,sf[i+1]-v[i]);
         }
         return ans;
     }
